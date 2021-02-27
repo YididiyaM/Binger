662eba9d2f1df73337fe049a7e0d534e2212f7bf
Yididiya Mulatu <yididiyamulatu@gmail.com>
2021-02-27 23:28:55 +0100

    trying to connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,16 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
 const bodyParser = require('body-parser'); 
-
+const { default: userEvent } = require('@testing-library/user-event');
 const app = express(); 
+const User = require("./user");
+
+mongoose.connect("mongodb+srv://bingerUser:<bingerPassword>@cluster0.ibeyl.mongodb.net/Binger?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, ()=>{
+    console.log("Mongoose is Connected")
+})
 
 //Middleware 
 app.use(bodyParser.json());
@@ -29,10 +37,21 @@ app.post('/login', (req,res)=>{
     console.log(req.body);
 })
 app.post('/register', (req,res)=>{
-    console.log(req.body);
+    User.findOne({username: req.body.username}, async (err, doc)=>{
+        if(err) throw err;
+        if(doc) res.send("User already exists");
+        if(!doc) {
+            const newUser = new User ({
+                username: req.body.username,
+                password: req.body.password,
+            });
+            await newUser.save();
+            res.send("User Created");
+        }
+    })
 })
 app.get('/user', (req,res)=> {
-
+    res.send(req.user)
 })
 app.listen(4000, ()=> {
     console.log('Server Has Started')
--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import {useState} from 'react';
+import Axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const [registerUsername, setRegisterUsername] = useState('');
+const [registerPassword, setRegisterPassword] = useState('');
+const [loginUsername, setLoginUsername] = useState('');
+const [loginPassword, setLoginPassword] = useState('');
+
+const register = () => {
+  Axios ({
+    method: "POST",
+    data: {
+      username: registerUsername,
+      password: registerPassword,
+    },
+    withCredentials: true,
+    url: "http://localhost:4000/register",
+  }).then((res)=> console.log(res));
+};
+const login = () => {
+  Axios ({
+    method: "POST",
+    data: {
+      username: loginUsername,
+      password: loginPassword,
+    },
+    withCredentials: true,
+    url: "http://localhost:4000/login",
+  }).then((res)=> console.log(res));
+};
+const getUser = () => {
+  Axios ({
+    method: "GET",
+    withCredentials: true,
+    url: "http://localhost:4000/getUser",
+  }).then((res)=> console.log(res));
+};
+
   return (
     <div className="App">
       <main> 
@@ -11,7 +48,27 @@ function App() {
        </div>
      </section>
      <div className="headline"> 
-      <h1>Binger</h1>
+      
+      <div>
+        <h1> Login</h1>
+        <input placeholder="username" onChange={e=> setLoginUsername(e.target.value)}/>
+        <input placeholder="password" onChange={e=> setLoginPassword(e.target.value)}/>
+        <button onClick ={login}> Submit </button>
+
+      </div>
+      <div>
+        <h1> Register</h1>
+        <input placeholder="username" onChange = {e => setRegisterUsername(e.target.value)}/>
+        <input placeholder="password" onChange = {e => setRegisterPassword(e.target.value)}/>
+        <button onClick ={register}> Submit </button>
+        
+        
+
+      </div>
+      <div>
+        <h1> Get User </h1>
+        <button onClick={getUser}> Submit </button>
+      </div>
       </div>
      </main>
      
