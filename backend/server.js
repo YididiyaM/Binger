fbbacb31fbd3313bb4633721d9545f5f1cca7e46
Yididiya Mulatu <yididiyamulatu@gmail.com>
2021-03-13 09:55:27 +0100

    connected backend to frontend

--- backend/server.js
+++ backend/server.js
@@ -1,58 +1,50 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const cors = require('cors');
-const password = require ('passport');
-const passportLocal = require ('passport-local').Strategy;
-const cookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt');
-const session = require('express-session');
-const bodyParser = require('body-parser'); 
-const { default: userEvent } = require('@testing-library/user-event');
-const app = express(); 
-const User = require("./user");
+const cors = require ('cors');
+const passport= require('passport-local');
+const passportLocal = require('passport-local')
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 
-mongoose.connect("mongodb+srv://bingerUser:<bingerPassword>@cluster0.ibeyl.mongodb.net/Binger?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}, ()=>{
-    console.log("Mongoose is Connected")
-})
+const app = express();
 
-//Middleware 
+//Middleware
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(cors({
-    origin: "http://localhost:3000", 
-    credentials: true
-}))
-app.use(session({
-    secret:"secretcode",
-    resave: true,
-    saveUninitialized: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(cookieParser("secretcode"))
-//routes
-app.post('/login', (req,res)=>{
-    console.log(req.body);
-})
-app.post('/register', (req,res)=>{
-    User.findOne({username: req.body.username}, async (err, doc)=>{
-        if(err) throw err;
-        if(doc) res.send("User already exists");
-        if(!doc) {
-            const newUser = new User ({
-                username: req.body.username,
-                password: req.body.password,
-            });
-            await newUser.save();
-            res.send("User Created");
-        }
+app.use(
+    cors({
+      origin: "http://localhost:3000", // <-- location of the react app were connecting to
+      credentials: true,
     })
-})
-app.get('/user', (req,res)=> {
-    res.send(req.user)
-})
-app.listen(4000, ()=> {
-    console.log('Server Has Started')
+  );
+  app.use(
+    session({
+      secret: "secretcode",
+      resave: true,
+      saveUninitialized: true,
+    })
+  );
+  app.use(cookieParser("secretcode"));
+  
+  //Routes
+
+  app.post('/register', (req,res)=> {
+      console.log(req.body)
+  })
+
+  app.post('/login', (req,res)=> {
+      console.log(req.body)
+  })
+
+  app.get('/user', (req,res) => {
+
+  })
+
+  //Start server
+app.listen(4000, ()=>{
+    console.log('Server has started')
 })
---
+++ src/Components/Templates/LoginPage.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import Axios from 'axios';
+
+function LoginPage(){
+const [registerUsername, setRegisterUsername] = useState("");
+const [registerPassword, setRegisterPassword] = useState("");
+const [loginUsername, setLoginUsername] = useState("");
+const [loginPassword, setLoginPassword] = useState("");
+
+const register = () => {
+    Axios({
+      method: "POST",
+      data: {
+        username: registerUsername,
+        password: registerPassword,
+      },
+      withCredentials: true,
+      url: "http://localhost:4000/register",
+    }).then((res) => console.log(res));
+  };
+  const login = () => {
+    Axios({
+      method: "POST",
+      data: {
+        username: loginUsername,
+        password: loginPassword,
+      },
+      withCredentials: true,
+      url: "http://localhost:4000/login",
+    }).then((res) => console.log(res));
+  };
+
+return (
+    <div className="App">
+    <main> 
+   <section className="glass">
+     <div className="register"> 
+     <h1> Register </h1>
+     <input placeholder="username"/> 
+     <input placeholder="password"/> 
+     <button onClick={register}>Submit</button>
+     </div>
+   </section>
+
+   <section>
+   <div>
+        <h1>Login</h1>
+        <input
+          placeholder="username"
+          onChange={(e) => setLoginUsername(e.target.value)}
+        />
+        <input
+          placeholder="password"
+          onChange={(e) => setLoginPassword(e.target.value)}
+        />
+        <button onClick={login}>Submit</button>
+      </div>
+   </section>
+   <div className="headline"> 
+    <h1>Binger</h1>
+    </div>
+   </main>
+   
+  
+   <div className="circle1">  </div>
+   <div className="circle2">  </div>
+   <div className="circle3">  </div>
+   
+  </div>
+)
+
+}
+
+
+
+export default LoginPage; 
