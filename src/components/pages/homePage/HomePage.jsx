c6f4b97d9121f72fde60f7bbd5d4100d84179c38
Yididiya Mulatu <yididiyamulatu@gmail.com>
2021-03-18 23:34:45 +0100

    usernames sort of fetched on frontend for profile

--- src/components/pages/homePage/HomePage.jsx
+++ src/components/pages/homePage/HomePage.jsx
@@ -1,4 +1,19 @@
+import React, { useState, useEffect } from "react";
 function HomePage() {
+  const [users, setUsers] = useState([]);
+  const url = "http://localhost:4000/usernames";
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(url, { mode: "cors" });
+      // console.log(response);
+      const data = await response.json();
+      console.log(data);
+      setUsers(data);
+    };
+    getData();
+    console.log(getData);
+  }, []);
+
   return (
     <div className="classname">
       <h1>Home Page </h1>
