11c6e8b7382f43ee4b6a98963697fa0c02d37fb4
Yididiya Mulatu <yididiyamulatu@gmail.com>
2021-03-19 19:11:23 +0100

    Hid the array before the search

--- src/components/pages/homePage/UserSearch.jsx
+++ src/components/pages/homePage/UserSearch.jsx
@@ -2,26 +2,32 @@ import React, { useState, useEffect } from "react";
 
 export default function UserSearch({ users }) {
   const [searchName, setSearchName] = useState("");
+  const [searchResult, setSearchResult] = useState("");
   const names = users.map((name) => {
     return name.username;
   });
-  console.log(names);
+
+  useEffect(() => {
+    const results = names.filter((val) => {
+      if (searchName == "") {
+        return "No user by that name";
+      } else if (val.toLowerCase().includes(searchName.toLowerCase())) {
+        return val;
+      }
+    });
+    setSearchResult(results);
+  }, [searchName]);
   return (
     <div>
       <input
         type="text"
+        value={searchName}
         placeholder="search username here"
         onChange={(event) => {
           setSearchName(event.target.value);
         }}
       />
-      {names.filter((val) => {
-        if (searchName == "") {
-          return "No user by that name";
-        } else if (val.toLowerCase().includes(searchName.toLowerCase())) {
-          return val;
-        }
-      })}
+      <div className="results">{searchResult}</div>
     </div>
   );
 }
